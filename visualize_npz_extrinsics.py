82c2291ef1a49f49c7f1b7e2c70053322afe347d
lyg1597 <li213@illinois.edu>
2025-07-23 10:47:20 -0700

    improved visualization

--- visualize_npz_extrinsics.py
+++ visualize_npz_extrinsics.py
@@ -3,6 +3,37 @@ import pyvista as pv
 import argparse
 import os
 
+def visualize_point_cloud(plotter, points, colors):
+    """
+    Creates an interactive 3D plot of the transformed point cloud.
+
+    Args:
+        points (np.ndarray): The point cloud in the JSON frame.
+        colors (np.ndarray): The RGB colors for the points.
+    """
+    print("Opening visualization window...")
+    cloud = pv.PolyData(points[::20,:])
+    cloud["colors"] = colors[::20,:]
+
+    # plotter = pv.Plotter(window_size=[1200, 800])
+    plotter.add_mesh(
+        cloud,
+        render_points_as_spheres=True,
+        point_size=5,
+        scalars="colors",
+        rgb=True,
+        ambient=1.0,
+        show_edges=False, 
+        lighting=False,
+    )
+    plotter.add_title("Transformed Point Cloud (JSON Frame)", font_size=12)
+    # plotter.enable_eye_dome_lighting()
+    plotter.add_axes()
+    print("Close the PyVista window to exit the script.")
+    # plotter.show()
+    return plotter
+
+
 def visualize_scene(npz_file_path: str, axis_scale: float = 0.1):
     """
     Loads a scene from an .npz file and visualizes the point cloud
@@ -20,13 +51,16 @@ def visualize_scene(npz_file_path: str, axis_scale: float = 0.1):
     print(f"Loading scene data from {npz_file_path}...")
     data = np.load(npz_file_path)
 
-    point_cloud_vertices = data.get('point_cloud')
-    point_cloud_color = data.get('point_cloud_color')
-    extrinsics = data.get('extrinsics')
-    world_to_camera = np.zeros((extrinsics.shape[0],4,4))
-    world_to_camera[:,:3,:] = extrinsics 
-    world_to_camera[:,3,3] = 1
-    camera_to_world = np.linalg.inv(world_to_camera)
+    point_cloud_vertices = data.get('raw_point_cloud').reshape((-1,3))
+    point_cloud_color = data.get('raw_point_color').reshape((-1,3))
+    if 'extrinsics' in data:
+        extrinsics = data.get('extrinsics')
+        world_to_camera = np.zeros((extrinsics.shape[0],4,4))
+        world_to_camera[:,:3,:] = extrinsics 
+        world_to_camera[:,3,3] = 1
+        camera_to_world = np.linalg.inv(world_to_camera)
+    else:
+        camera_to_world = np.array([])
     
     p = pv.Plotter()
     p.background_color = "white"
@@ -36,6 +70,8 @@ def visualize_scene(npz_file_path: str, axis_scale: float = 0.1):
     p.add_mesh(pv.Line((0, 0, 0), (0, 2, 0)), color="green", line_width=4)
     p.add_mesh(pv.Line((0, 0, 0), (0, 0, 2)), color="blue",  line_width=4)
 
+    p = visualize_point_cloud(p, point_cloud_vertices, point_cloud_color)
+ 
     for i in range(camera_to_world.shape[0]):           # rgb_path unused here
 
         # Full camera rotation with –5° pitch offset applied
